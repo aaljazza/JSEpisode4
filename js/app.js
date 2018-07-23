5e2e018ca9513133465a232be28f1bb1efea7d00
Octowl <alsaff1987@gmail.com>
2018-07-23 13:34:55 +0300

    Export functions for testing

--- js/app.js
+++ js/app.js
@@ -7,41 +7,40 @@ const addButton = document.getElementById("add-button");
 const todoList = document.getElementById("todo-list");
 
 /****************************************
-* The following loop adds event handlers
-* to the already existing tasks
-* DO NOT EDIT THIS CODE!!!
-****************************************/
+ * The following loop adds event handlers
+ * to the already existing tasks
+ * DO NOT EDIT THIS CODE!!!
+ ****************************************/
 Array.from(todoList.children).forEach(function(listItem) {
-	let checkBox = listItem.querySelector("input[type=checkbox]"); //
-	let editButton = listItem.querySelector("button.edit"); //
-	editButton.onclick = editTask; // Bind editTask to edit button
-	checkBox.onchange = completeTask;
+  let checkBox = listItem.querySelector("input[type=checkbox]"); //
+  let editButton = listItem.querySelector("button.edit"); //
+  editButton.onclick = editTask; // Bind editTask to edit button
+  checkBox.onchange = completeTask;
 });
 
 // Add a click handler to the addButton
 addButton.onclick = addTask;
 
-
 /*******************************************
-* Create a new to-do item:
-*
-* Generates a new to-do list item along
-* with all of its buttons and input fields
-*
-* createNewTask(itemString);
-********************************************/
+ * Create a new to-do item:
+ *
+ * Generates a new to-do list item along
+ * with all of its buttons and input fields
+ *
+ * createNewTask(itemString);
+ ********************************************/
 function createNewTask(taskString) {
-	let listItem = document.createElement("li"); // Create List Item
-	let checkBox = document.createElement("input"); // Input (checkbox)
-	let label = document.createElement("label"); // Label
-	let editInput = document.createElement("input"); // Input (text)
-	let editButton = document.createElement("button"); // Button.edit
+  let listItem = document.createElement("li"); // Create List Item
+  let checkBox = document.createElement("input"); // Input (checkbox)
+  let label = document.createElement("label"); // Label
+  let editInput = document.createElement("input"); // Input (text)
+  let editButton = document.createElement("button"); // Button.edit
 
-	checkBox.type = "checkbox"; // Make this input a checkbox
-	editInput.type = "text"; // Make this input a text field
-	editButton.innerText = "Edit"; // Change the text on the button
-	editButton.className = "edit"; // Give the button a .edit class
-	label.innerText = taskString; // Change the label text to the new taskString
+  checkBox.type = "checkbox"; // Make this input a checkbox
+  editInput.type = "text"; // Make this input a text field
+  editButton.innerText = "Edit"; // Change the text on the button
+  editButton.className = "edit"; // Give the button a .edit class
+  label.innerText = taskString; // Change the label text to the new taskString
 
   // COMPLETE ME!
   // Add handlers for the edit button and checkbox
@@ -49,53 +48,57 @@ function createNewTask(taskString) {
   // checkBox.onchange = ...
 
   // Append each element to the listItem
-	listItem.appendChild(checkBox);
-	listItem.appendChild(label);
-	listItem.appendChild(editInput);
-	listItem.appendChild(editButton);
+  listItem.appendChild(checkBox);
+  listItem.appendChild(label);
+  listItem.appendChild(editInput);
+  listItem.appendChild(editButton);
 
-	return listItem;
-};
-
+  return listItem;
+}
 
 /*****************************************************
-* Add a new task to the list:
-* - Create a new task with the value from taskInput
-* - If there is nothing in taskInput, the default is "New Task"
-* - Append the new task to the todoList
-* - Reset the value of taskInput
-*****************************************************/
+ * Add a new task to the list:
+ * - Create a new task with the value from taskInput
+ * - If there is nothing in taskInput, the default is "New Task"
+ * - Append the new task to the todoList
+ * - Reset the value of taskInput
+ *****************************************************/
 function addTask() {
-	// Complete me!
-};
-
+  // Complete me!
+}
 
 /*****************************************************************
-* Edit a task:
-* - Get the current list item
-* - Get the label and the input field from the list item
-* - Check if the list item is in edit mode:
-*   - Edit Mode:
-*       - set the label text to the value of the input field
-*       - set the button text to 'Edit'
-*   - !Edit Mode:
-*       - set the input field value to the text of the label
-*       - set the button text to 'Save'
-* - Toggle edit mode
-*****************************************************************/
+ * Edit a task:
+ * - Get the current list item
+ * - Get the label and the input field from the list item
+ * - Check if the list item is in edit mode:
+ *   - Edit Mode:
+ *       - set the label text to the value of the input field
+ *       - set the button text to 'Edit'
+ *   - !Edit Mode:
+ *       - set the input field value to the text of the label
+ *       - set the button text to 'Save'
+ * - Toggle edit mode
+ *****************************************************************/
 function editTask() {
   // get the current list item which is the parent
   // node of the current button (`this`)
-	let listItem = this.parentNode;
+  let listItem = this.parentNode;
   // Complete me!
-};
-
+}
 
 /***********************************
-* Mark a task as completed:
-* - Get the current list item
-* - Remove the item from todoList
-***********************************/
+ * Mark a task as completed:
+ * - Get the current list item
+ * - Remove the item from todoList
+ ***********************************/
 function completeTask() {
   // Complete me!
+}
+
+module.exports = {
+  createNewTask,
+  addTask,
+  editTask,
+  completeTask
 };
